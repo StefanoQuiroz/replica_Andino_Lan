1904c11981439873f6453bea428ee64cd756f536
Stefano <piero.quiroz93@gmail.com>
2021-07-27 11:28:51 -0500

    Commit inicial para nuevo 'Ejemplo.js'

--- andino/src/App.js
+++ andino/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-
+import Ejemplo from './components/Ejemplo'
 function App() {
   return (
     <div className="App">
-      
+      <Ejemplo/>
     </div>
   );
 }
---
+++ andino/src/components/Ejemplo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Ejemplo = () => {
+    return (
+        <div>
+            <h1>Hola a mi mismo</h1>
+        </div>
+    );
+}
+
+export default Ejemplo;
