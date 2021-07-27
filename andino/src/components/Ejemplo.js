e62ed132ae82b840b5284f2004b91c787f3221d2
Stefano <piero.quiroz93@gmail.com>
2021-07-27 12:02:18 -0500

    agregue nuevo texto antes del de Joaco

--- andino/src/components/Ejemplo.js
+++ andino/src/components/Ejemplo.js
@@ -4,6 +4,7 @@ const Ejemplo = () => {
     return (
         <div>
             <h1>Hola a mi mismo</h1>
+            <p>quiero ser un teletubbie y correr calato por el campo (si lo haría)</p>
             <p>Ipsi dupsi tipsi</p>
         </div>
     );
