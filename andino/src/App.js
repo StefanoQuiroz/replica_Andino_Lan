11c5a167434b0ff7a1006cf686ab408ac02f5f75
JoacoMaurtua <joacomaurtua@gmail.com>
2021-07-28 12:16:00 -0500

    Agregando el carrusel pero con errores

--- andino/src/App.js
+++ andino/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
+import HeroSection from './components/HeroSection';
 
 function App() {
   return (
     <div className="App">
-      
+      <HeroSection/>
       
     </div>
   );
---
+++ andino/src/components/HeroSection.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import { useState } from 'react';
+
+import {  
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption
+} from 'reactstrap';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const items = [
+  {
+    src: require('../images/machu.png'),
+    altText: 'imagen 1',
+    caption: 'Machu Pichu'
+  },
+
+  {
+    src: require('../images/colas.png'),
+    altText: 'imagen 2',
+    caption: 'coca colas'
+  }
+  
+];
+
+export default function HeroSection(props) {
+
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [animating, setAnimating] = useState(false);
+
+  const next = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+    setActiveIndex(nextIndex);
+  }
+
+  const previous = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+    setActiveIndex(nextIndex);
+  }
+
+  const goToIndex = (newIndex) => {
+    if (animating) return;
+    setActiveIndex(newIndex);
+  }
+
+  const slides = items.map((item) => {
+    return (
+      <CarouselItem
+        onExiting={() => setAnimating(true)}
+        onExited={() => setAnimating(false)}
+        key={item.src}
+      >
+        <img src={item.src} alt={item.altText} width="100%" height="750px"/>
+        <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
+      </CarouselItem>
+    );
+  });
+
+  return (
+    <Carousel
+      activeIndex={activeIndex}
+      next={next}
+      previous={previous}
+    >
+      <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
+      {slides}
+      <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
+      <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
+    </Carousel>
+  );
+
+}
+
+
