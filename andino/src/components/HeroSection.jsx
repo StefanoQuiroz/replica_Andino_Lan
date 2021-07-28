42ffe9ef988f191aefcefd538240bd62300b25ca
JoacoMaurtua <joacomaurtua@gmail.com>
2021-07-28 15:25:09 -0500

    regresando a la version anterior

--- andino/src/components/HeroSection.jsx
+++ andino/src/components/HeroSection.jsx
@@ -1,27 +1,30 @@
 import React from 'react';
 import { useState } from 'react';
+import Machu from '../images/machu-picchu.jpg';
+import Cocas from '../images/cocas.jpg';
 
-import {  
+import { 
+  Row,
+  Col, 
+  Container,
   Carousel,
   CarouselItem,
   CarouselControl,
   CarouselIndicators,
-  CarouselCaption
+  CarouselCaption,
+  Button
 } from 'reactstrap';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { left } from '@popperjs/core';
 
 const items = [
   {
-    src: require('../images/machu.png'),
-    altText: 'imagen 1',
-    caption: 'Machu Pichu'
+    src: Machu
   },
 
   {
-    src: require('../images/colas.png'),
-    altText: 'imagen 2',
-    caption: 'coca colas'
+    src: Cocas 
   }
   
 ];
@@ -55,23 +58,49 @@ export default function HeroSection(props) {
         onExited={() => setAnimating(false)}
         key={item.src}
       >
-        <img src={item.src} alt={item.altText} width="100%" height="750px"/>
+        <img src={item.src} alt={item.altText} width="830" height="340"/>
         <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
       </CarouselItem>
     );
   });
 
   return (
-    <Carousel
-      activeIndex={activeIndex}
-      next={next}
-      previous={previous}
-    >
-      <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
-      {slides}
-      <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
-      <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
-    </Carousel>
+    <Container style={{marginTop:'10px'}}>
+      
+      <Row style={{width:"98.5%"}}>
+        <p className="breadCrumb">Inicio</p>
+      </Row>
+
+      <Row>
+        <Col sm={8} style={{padding:"0",marginRight:"-18px"}}>
+            <Carousel
+            activeIndex={activeIndex}
+            next={next}
+            previous={previous}
+          >
+            <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
+            {slides}
+            <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
+            <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
+          </Carousel>
+        </Col>
+        <Col sm={4}>
+          <Row className="zone-form">
+            <p>
+              Para consultas de los documentos electrónicos de nuestros clientes
+            </p>
+            <Button style={{width:'50%', height:'2.5rem', 
+                           margin:'0 auto', marginTop:'-50px',
+                           fontSize:'20px', background:'#030e3c',}}
+            >Consulta aqui</Button>
+          </Row>
+        
+        </Col>
+      </Row>
+       
+    </Container>
+
+ 
   );
 
 }
--- andino/src/components/NavBar.jsx
+++ andino/src/components/NavBar.jsx
@@ -1,32 +1,9 @@
-import React from 'react';
-import { NavbarBrand, Navbar, Nav, NavItem, NavLink } from 'reactstrap';
-import logo from '../images/Logo.PNG';
-//#ded101 url(bg_header.jpg) top center
-const NavBar = () => {
-  return (
-   /*  <Container> */
-      <Navbar style={{boxShadow: '5px 4px 8px #888888'}} className="NavBar" color="light" light expand="md">
-        <NavbarBrand><img src={logo}/></NavbarBrand>
-        <Nav navbar>
-          <NavItem>
-            <NavLink>INICIO</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink>PRODUCTOS</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink>SUCURSALES</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink>UBICACIÓN</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink>CONTACTO</NavLink>
-          </NavItem>
-        </Nav>
-      </Navbar>
-    /* </Container> */
-  );
-}
+import React from 'react'
 
-export default NavBar;
+export default function NavBar() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
