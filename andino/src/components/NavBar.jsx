61e277edf2475a790edcd5c282b4c0f5940ad754
Stefano <piero.quiroz93@gmail.com>
2021-07-30 09:57:10 -0500

    Modificación prueba test nuevos pasos Stefano

--- andino/src/components/NavBar.jsx
+++ andino/src/components/NavBar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavbarBrand, Navbar, Nav, NavItem, NavLink } from 'reactstrap';
 import logo from '../images/Logo.PNG';
 //#ded101 url(bg_header.jpg) top center
+//Hice una modificación
 const NavBar = () => {
   return (
       <Navbar style={{boxShadow: '5px 4px 8px #888888'}} className="NavBar" color="light" light expand="md">
