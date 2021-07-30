8b8634b807a2b857bd9331cc477667f44fea3a4f
Stefano <piero.quiroz93@gmail.com>
2021-07-30 13:31:34 -0500

    Footer Terminado

--- andino/src/App.js
+++ andino/src/App.js
@@ -2,15 +2,16 @@ import React from 'react';
 import './App.css';
 import HeroSection from './components/HeroSection';
 import Products from './components/Products';
-
 import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>  
       <HeroSection/>
-      <Products/>  
+      <Products/>
+      <Footer/>  
     </div>
   );
 }
