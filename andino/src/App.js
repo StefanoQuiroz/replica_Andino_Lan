7f6757c1d32a5c6635b91d6d2323075a263f6c5a
Stefano <piero.quiroz93@gmail.com>
2021-07-30 22:14:49 -0500

    Se creo el routing de la página

--- andino/src/App.js
+++ andino/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
 import Inicio from './views/Inicio';
-// 
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Productos from './views/Productos';
 function App() {
   return (
     <div className="App">
-      <Inicio/>
+      <Router>
+        <Switch>
+          <Route exact path={`/`}>
+            <Inicio/>
+          </Route>
+          <Route path={`/productos`}>
+            <Productos/>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
